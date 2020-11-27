111a1f41fd240dff3b2ea8c214a393d36c251450
ZhouFang92 <75120101+ZhouFang92@users.noreply.github.com>
2020-11-27 12:42:39 +0100

    1

--- Simple gene expression model/drift_term.m
+++
@@ -1,6 +0,0 @@
-function a=drift_term(t,x) %x1 corresponds to X_3 (RNA) and x2 corresponds to protein.
-
-k4=0.390;
-
-k6=0.379;
-a=[0;0; 0;k4*x(3)-k6*x(4)];
--- Transcription regulation network/propensity_reduced_system.m
+++
@@ -1,9 +0,0 @@
-function a=propensity_reduced_system(X,j,K)
-
-if j==3
-    a=K(3)*K(5)*psi_function(X,K)*(X(4)+X(5))/(K(5)*psi_function(X,K)+K(6));
-end
-
-if j==4
-   a=K(4)*X(3);
-end
